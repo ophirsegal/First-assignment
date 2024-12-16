4f64e13f9bb02566f33899aad94472a1b699c04f
segalophir <segalophir@gmail.com>
2024-12-16 14:57:12 +0200

    test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const initApp = require("./server");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // default port
 
 initApp.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
